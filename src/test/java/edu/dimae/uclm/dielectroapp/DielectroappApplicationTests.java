b39c22cab9160b4c9301ae68f04769a3e014f12a
fcocrespo <Fco.Crespo@alu.uclm.es>
2022-11-15 09:23:10 +0100

    fisrt commit

---
+++ src/test/java/edu/dimae/uclm/dielectroapp/DielectroappApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dimae.uclm.dielectroapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DielectroappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
